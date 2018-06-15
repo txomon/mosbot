9687a8e9fcb50ea2cca7c54cd36fa922ffbb4f10
Javier Domingo Cansino <javierdo1@gmail.com>
2018-06-15 13:28:11 +0200

    fix: Assert passed types for save_user and update unit tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import pytest
 
-from mosbot.query import save_user, save_track
+from mosbot.query import get_user, save_user, save_track
 
 
 @pytest.mark.asyncio
@@ -19,23 +19,30 @@ async def test_save_user(db_conn):
 
     # correct user
     user_dict = {'username': 'The User', 'dtid': '0123456789theuser'}
-    result = await save_user(user_dict=user_dict, conn=db_conn)
-    assert dict(id=1, **user_dict) == result
+    actual_result = await save_user(user_dict=user_dict, conn=db_conn)
+    expected_result = dict(id=1, **user_dict)
+    assert expected_result == actual_result
 
     # duplicated user
-    result = await save_user(user_dict=user_dict, conn=db_conn)
-    assert user_dict == result
+    actual_result = await save_user(user_dict=user_dict, conn=db_conn)
+    assert expected_result == actual_result
 
-    # corrupted user
+    # corrupted user.username (should be a string)
     user_dict = {'username': 1982, 'dtid': '0123456789theuser'}
-    result = await save_user(user_dict=user_dict, conn=db_conn)
-    assert user_dict == result
+    with pytest.raises(Exception):
+        await save_user(user_dict=user_dict, conn=db_conn)
+
+    # corrupted user.dtid (should be a string)
+    user_dict = {'username': '1982', 'dtid': 123456789}
+    with pytest.raises(Exception):
+        await save_user(user_dict=user_dict, conn=db_conn)
 
 
 @pytest.mark.asyncio
 async def test_save_track(db_conn):
     # empty track_dict
-    result = await save_track(track_dict={}, conn=db_conn)
+    result = await save_track(track_dict=True, conn=db_conn)
+    # result = await save_track(track_dict={}, conn=db_conn)
     assert result is None
 
     # correct track_dict
