19147d5ebe5c1875b9c3850ab8b66d8feebbd25c
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-12-21 13:33:34 +0000

    Minor typo

--- mosbot/__main__.py
+++ mosbot/__main__.py
@@ -6,7 +6,7 @@ import click
 from mosbot.command import botcli, botcmd
 
 main = click.CommandCollection(sources=[botcli, botcmd])
-"""This is groups all the commands under one so that we can have them together, regardless of the implementation"""
+"""This groups all the commands under one so that we can have them together, regardless of the implementation"""
 
 if __name__ == '__main__':  # pragma: no cover
     main()
